06fe16e048d62d50c549d9867a8190067d15187e
Yggdrasill <yggdrasill@shitpost.is>
2015-02-02 18:44:00 +0100

    set DATABASE_PATH in config.py to generic /path/to/db

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 import base64
 
-DATABASE_PATH="/tmp/test.db"
+DATABASE_PATH="/path/to/db"
 
 SECRET_KEY = base64.b64encode(os.urandom(10) )
 SQLALCHEMY_DATABASE_URI = "sqlite:///"+DATABASE_PATH
